e4c8e1b746da49db69c1e4dc4008b45f162c4e5f
August Kilponen <august.kilponen@gmail.com>
2020-07-16 15:20:28 +0300

    Generoitu uudet kk-varasijakirjeet

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -45,8 +45,9 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "kk_hyvaksymiskirje_syksy_2018,kk_jalkiohjauskirje_syksy_2018";
         //String templateKeys = "kk_varasijakirje_2018";
         //String templateKeys = "kk_ensimmainen_varasijakirje_2020";
-        String templateKeys = "kk_toinen_hyvaksymiskirje_2020";
+        //String templateKeys = "kk_toinen_hyvaksymiskirje_2020";
         //String templateKeys = "kk_toinen_jalkiohjauskirje_2020";
+        String templateKeys = "kk_toinen_varasijakirje_2020";
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
                 for (String language : asList("FI", "SV", "EN")) {
