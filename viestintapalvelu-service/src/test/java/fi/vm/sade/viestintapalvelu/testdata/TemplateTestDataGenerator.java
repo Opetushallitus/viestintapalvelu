634b19f932a420af593042f8bc5ba60c02e2bfde
August Kilponen <august.kilponen@gmail.com>
2021-06-03 14:15:24 +0300

    Generoitu kirjeitä

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -37,7 +37,8 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "omattiedot_email";
 //        String templateKeys = "2aste_hyvaksymiskirje_2021,2aste_jalkiohjauskirje_2021";
 //        String templateKeys = "erkka_hyvaksymiskirje_2021,erkka_jalkiohjauskirje_2021";
-        String templateKeys = "nivel_hyvaksymiskirje_2021,nivel_jalkiohjauskirje_2021";
+        //String templateKeys = "nivel_hyvaksymiskirje_2021,nivel_jalkiohjauskirje_2021";
+        String templateKeys = "kk_ensimmainen_hyvaksymiskirje_2021,kk_ensimmainen_jalkiohjauskirje_2021,kk_toinen_hyvaksymiskirje_2021,kk_toinen_jalkiohjauskirje_2021";
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
                 for (String language : asList("FI", "SV", "EN")) {
