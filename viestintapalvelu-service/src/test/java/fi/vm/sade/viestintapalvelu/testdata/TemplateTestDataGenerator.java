bffd1cb3e7271b7ffb59dea2d641b4229b597cf7
August Kilponen <august.kilponen@gmail.com>
2022-06-09 14:24:01 +0300

    Generoitu kk-kirjeet

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -38,7 +38,8 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "omattiedot_email_2aste";
         //String templateKeys = "2aste_koekutsukirje_2022";
         //String templateKeys = "2aste_hyvaksymiskirje_2022,2aste_hyvaksymiskirje_huoltajille_2022";
-        String templateKeys = "2aste_jalkiohjauskirje_2022,2aste_jalkiohjauskirje_huoltajille_2022";
+        //String templateKeys = "2aste_jalkiohjauskirje_2022,2aste_jalkiohjauskirje_huoltajille_2022";
+        String templateKeys = "kk_ensimmainen_hyvaksymiskirje_2022,kk_ensimmainen_jalkiohjauskirje_2022,kk_ensimmainen_varasijakirje_2022";
 
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
