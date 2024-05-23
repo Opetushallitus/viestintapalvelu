c3847e7989d06a235767949194883ae267c169f2
August Kilponen <august.kilponen@gmail.com>
2024-05-23 13:34:30 +0300

    Generoitu kirjeitä

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -46,7 +46,8 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "kk_ensimmainen_varasijakirje_2023";
         //String templateKeys = "2aste_hyvaksymiskirje_2023,2aste_jalkiohjauskirje_2023,2aste_hyvaksymiskirje_huoltajille_2023,2aste_jalkiohjauskirje_huoltajille_2023";
         //String templateKeys = "kk_toinen_hyvaksymiskirje_2023,kk_toinen_jalkiohjauskirje_2023";
-        String templateKeys = "kk_jalkiohjauskirje_syksy_2023,kk_hyvaksymiskirje_syksy_2023,kk_varasijakirje_syksy_2023";
+        //String templateKeys = "kk_jalkiohjauskirje_syksy_2023,kk_hyvaksymiskirje_syksy_2023,kk_varasijakirje_syksy_2023";
+        String templateKeys = "kk_ensimmainen_hyvaksymiskirje_2024,kk_ensimmainen_jalkiohjauskirje_2024";
 
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
